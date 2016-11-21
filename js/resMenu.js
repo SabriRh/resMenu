f90915ce508e7ef7c808954b0cdfd8cee98aa2cd
SabriRh <sabri.rhaimia@digit-u.com>
2016-11-21 17:56:40 +0100

    dynamic annimations for menu + fix add options multiple times to mobile menu

--- js/resMenu.js
+++ js/resMenu.js
@@ -1,56 +1,92 @@
+init();
+
+
+
+function init() {
+
+    //avilables annimations :
+
+
+    /*resMenuFadeIn
+    resMenuBounceIn
+    resMenuBounceInLeft
+    resMenuBounceInRight
+    resMenuBounceInUp
+    resMenuBounceInDown*/
+
+    annimationEntrance = 'fadeIn'
+
+    if ($('#resMenu').hasClass('resMenuFadeIn')) {
+
+        annimationEntrance = 'fadeIn';
+
+    } else if ($('#resMenu').hasClass('resMenuBounceIn')) {
+        annimationEntrance = 'bounceIn';
+
+    } else
+    if ($('#resMenu').hasClass('resMenuBounceInLeft')) {
+        annimationEntrance = 'bounceInLeft';
+
+    } else if ($('#resMenu').hasClass('resMenuBounceInRight')) {
+        annimationEntrance = 'bounceInRight';
+
+    } else if ($('#resMenu').hasClass('resMenuBounceInUp')) {
+        annimationEntrance = 'bounceInUp';
+
+    } else if ($('#resMenu').hasClass('resMenuBounceInDown')) {
+        annimationEntrance = 'bounceInDown';
+
+    }
+
+
+    annimationExit = annimationEntrance.replace("In", "Out");
+
+    console.log(annimationEntrance, annimationExit);
+
+
+};
 
 function CreateMobileMenu() {
-	$('#resMenu li:not(".resMenuDisableMobile") a').clone().appendTo('#resMenuMobileContainer');
+    $('#resMenu li:not(".resMenuDisableMobile") a').clone().appendTo('#resMenuMobileContainer');
 }
 
 function empty() {
-	$('#resMenuMobileContainer #resMenu li a').remove();
+    $('#resMenuMobileContainer').empty();
 }
 
 
-$('#resMenuMobileButton').click(function (){
+$('#resMenuMobileButton').click(function() {
 
 
 
+    //launch mobile menu
+
+    if ($('#resMenuMobileContainer').hasClass(annimationEntrance)) {
+        $('#resMenuMobileContainer').removeClass(annimationEntrance);
+        $('#resMenuMobileContainer').addClass('animated active ' + annimationExit);
+        $('#resMenuMobileContainer a').each(function() {
+            $(this).addClass('animated ' + annimationExit)
+        });
+        empty();
 
 
+    } else if ($('#resMenuMobileContainer').hasClass(annimationExit)) {
+        CreateMobileMenu();
+        $('#resMenuMobileContainer').removeClass(annimationExit);
+        $('#resMenuMobileContainer').addClass('animated active ' + annimationEntrance);
+        $('#resMenuMobileContainer a').each(function() {
+            $(this).addClass(annimationEntrance + ' animated')
+        });
+    } else {
+        CreateMobileMenu();
+        $('#resMenuMobileContainer').addClass('animated active ' + annimationEntrance);
+        $('#resMenuMobileContainer a').each(function() {
+            $(this).addClass(annimationEntrance + ' animated')
+        });
+    }
 
 
+    $(this).toggleClass('icon-menu icon-cancel');
 
 
-
-
-
-
-
-//launch mobile menu
-
-	if ($('#resMenuMobileContainer').hasClass('fadeIn')){
-		$('#resMenuMobileContainer').removeClass('fadeIn');
-		$('#resMenuMobileContainer').addClass('animated active fadeOut');
-		$('#resMenuMobileContainer a').each(function(){
-			$('this').addClass('animated fadeout')
-		});
-		empty();
-
-
-	}
-	else if ($('#resMenuMobileContainer').hasClass('fadeOut')){
-		CreateMobileMenu();
-		$('#resMenuMobileContainer').removeClass('fadeOut');
-		$('#resMenuMobileContainer').addClass('animated active fadeIn');
-		$('#resMenuMobileContainer a').each(function(){
-			$('this').addClass('fadeIn animated')
-		});
-	}
-	else{
-		CreateMobileMenu();
-		$('#resMenuMobileContainer').addClass('animated active fadeIn');
-		$('#resMenuMobileContainer a').each(function(){
-			$('this').addClass('fadeIn animated')
-		});
-	}
-
-
-}
-);
+});
